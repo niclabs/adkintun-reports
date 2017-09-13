edee5ea357ccfeb3c187460a6135c769005595ed
javier-op <javiermoralesr95@gmail.com>
2017-09-13 13:37:19 -0300

    Cambio fecha del cron para test.

--- app/automatization/scheduler_manager.py
+++ app/automatization/scheduler_manager.py
@@ -9,11 +9,12 @@ MAX_NUMBER_OF_QUERIES = 40
 
 
 # Job will be done the first day of every month
-@cron(20, 13, 13, -1, -1, target="mule")
+@cron(50, 13, 13, -1, -1, target="mule")
 def reports_generation():
     """
     Job updating frontend database and generating reports.
     :return:
     """
+    reportLogger.info(datetime.now().strftime("Monthly update is starting. - %H:%M %d/%m/%Y"))
     monthly_update()
-    reportLogger.info(datetime.now().strftime("Reports have been generated. - %H:%M %d/%m/%Y"))
+    reportLogger.info(datetime.now().strftime("Monthly update was completed. - %H:%M %d/%m/%Y"))
