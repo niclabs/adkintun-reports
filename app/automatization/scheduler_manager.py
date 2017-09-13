c2ab969cefb059223bfe3cb7420321881e077e4b
javier-op <javiermoralesr95@gmail.com>
2017-09-13 14:13:43 -0300

    Cambio fecha del cron para test.

--- app/automatization/scheduler_manager.py
+++ app/automatization/scheduler_manager.py
@@ -5,12 +5,10 @@ from uwsgidecorators import cron
 from app import reportLogger
 from app.automatization import monthly_update
 
-MAX_NUMBER_OF_QUERIES = 40
-
 
 # Job will be done the first day of every month
 @cron(50, 13, 13, -1, -1, target="mule")
-def reports_generation():
+def reports_generation(num: int):
     """
     Job updating frontend database and generating reports.
     :return:
