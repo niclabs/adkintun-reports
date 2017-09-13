06941473962de452ef3337ef1d55711cf72e1530
javier-op <javiermoralesr95@gmail.com>
2017-09-13 11:03:02 -0300

    Actualizo config.py

--- config.py
+++ config.py
@@ -2,22 +2,23 @@ class DefaultConfig(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    BIND_KEY = "frontend"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
     USER_SERVER = "this-should-be-changed"
     SECRET_KEY_SERVER = "this-should-be-changed"
     HOST_SERVER = "this-should-be-changed"
     DB_NAME_SERVER = "this-should-be-changed"
+    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(USER_SERVER, SECRET_KEY_SERVER,
+                                                                HOST_SERVER, DB_NAME_SERVER)
+    BIND_KEY = "frontend"
     USER_FRONTEND = "this-should-be-changed"
     SECRET_KEY_FRONTEND = "this-should-be-changed"
     HOST_FRONTEND = "this-should-be-changed"
     DB_NAME_FRONTEND = "this-should-be-changed"
-    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(USER_SERVER, SECRET_KEY_SERVER,
-                                                                HOST_SERVER, DB_NAME_SERVER)
     SQLALCHEMY_BINDS = {
         BIND_KEY: "postgresql://{}:{}@{}/{}".format(USER_FRONTEND, SECRET_KEY_FRONTEND,
                                                     HOST_FRONTEND, DB_NAME_FRONTEND)
     }
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class TestConfig(object):
