fc618d6ffbd84b4f503728b41166da73e3ade7db
javier-op <javiermoralesr95@gmail.com>
2017-09-15 13:45:32 -0300

    Cambio fecha cron para test.

--- app/report/views.py
+++ app/report/views.py
@@ -11,7 +11,7 @@ from app.public.views import page_not_found
 @application.route("/reports")
 @application.route("/reports/<path:path>")
 def reports(path="."):
-    return autoindex.render_autoindex(path=path, endpoint="reports", mimetype="application/json")
+    return autoindex.render_autoindex(path=path) #, endpoint="reports", mimetype="application/json")
 
 
 # reports urls for frontend
