80eb6a28ec87d634b1c660b69ecb46972ea5b399
javier-op <javiermoralesr95@gmail.com>
2017-09-22 11:44:48 -0300

    Actualizo views.

--- manage.py
+++ manage.py
@@ -1,10 +1,8 @@
 from flask_script import Manager
-
 from app import application
+from manage_commands import Test, ReportsGeneration, ReportsImportation
 
 manager = Manager(application)
-
-from manage_commands import Test, ReportsGeneration, ReportsImportation
 
 manager.add_command('test', Test())
 manager.add_command('generate_reports', ReportsGeneration())
