331255e43945d8bad8a6850b19a9da3d5c7ca22c
javier-op <javiermoralesr95@gmail.com>
2017-09-29 13:13:06 -0300

    Cambio fecha cron para test.

--- app/automatization/scheduler_manager.py
+++ app/automatization/scheduler_manager.py
@@ -7,7 +7,7 @@ from app.automatization import monthly_update
 
 
 # Job will be done the first day of every month
-@cron(20, 13, 29, 9, -1, target="mule")
+@cron(30, 13, 29, 9, -1, target="mule")
 def reports_generation(num: int):
     """
     Job updating frontend database and generating reports.
